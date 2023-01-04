53bfe632ecabd2897817dcd8f8b7cc607139b0e0
ali pourmoghadam <ali_pourmoghadam@protonmail.com>
2023-01-04 17:01:24 +0330

    feat : install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+import {createApp} from 'vue/dist/vue.esm-bundler.js'
+
+import AppComponent from "./components/AppComponent.vue"
+
+createApp({
+
+    components : {
+        AppComponent
+    }
+    
+}).mount("#app")
+
